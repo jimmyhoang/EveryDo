488f3e21db52bf07fea1f2781baff4a037814782
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-06-13 10:50:58 -0700

    Initial commit

---
+++ EveryDo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  EveryDo
+//
+//  Created by Jimmy Hoang on 2017-06-13.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ EveryDo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  EveryDo
+//
+//  Created by Jimmy Hoang on 2017-06-13.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
